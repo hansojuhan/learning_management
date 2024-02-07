65f0dc8282fa046514bc67fc645172ed371cf822
hje <hjerikson@gmail.com>
2024-02-07 18:31:06 +0100

    install active storage

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,6 @@
 class Course < ApplicationRecord
+  # Defines an image for the course that we can access in views
+  has_one_attached :image do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100,100]
+  end
 end
