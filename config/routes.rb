b9121c96f3d90c162454bec58e04c2de1c6eaeca
hje <hjerikson@gmail.com>
2024-02-07 17:56:25 +0100

    added admin authentication

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # Now can do /admin to get to index
+  get "admin" => "admin#index"
+
   # Defines the root path route ("/")
   root "courses#index"
 end
