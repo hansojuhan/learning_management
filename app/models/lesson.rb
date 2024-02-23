0a21559dc80af5efcbd34aef90d7ba2825018747
hje <hjerikson@gmail.com>
2024-02-23 19:53:26 +0100

    Started lessons page for admin

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,9 @@
 class Lesson < ApplicationRecord
   # After adding the video
-  has_one_attached :video
+  # This also adds a thumbnail for the video
+  has_one_attached :video do |attachable|
+    attachable.variant :thumb, resize_to_limit: [500,500]
+  end
 
   belongs_to :course
   has_many :lesson_users
