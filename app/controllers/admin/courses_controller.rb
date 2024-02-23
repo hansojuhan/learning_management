d16ff26a61d6270d59bc75e74d65662233729edc
hje <hjerikson@gmail.com>
2024-02-23 15:14:46 +0100

    Added admin course add view with form and styling, including RICH TEXT

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -1,6 +1,15 @@
-class Admin::CoursesController < ApplicationController
+class Admin::CoursesController < AdminController
+  # 1. At the moment, this is not protected by authentication control
+  # 2. Not using the admin layout
+  # Solution: instead of inheriting from ApplicationController
+  # Inherit from: AdminController
+
   def index
     @admin_courses = Course.all
   end
 
+  def new
+    @admin_course = Course.new
+  end
+  
 end
