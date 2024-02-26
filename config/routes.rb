11497c612afee6a00b0385fa474223c341555c0d
hje <hjerikson@gmail.com>
2024-02-26 18:20:48 +0100

    Added users table for admins along with delete that doesnt work

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,7 @@ Rails.application.routes.draw do
     resources :courses do
       resources :lessons
     end
+    resources :users
   end
 
   patch "/admin/course/:course_id/lessons/:id/move" => "admin/lessons#move"
